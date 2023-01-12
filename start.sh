e23817ef137236c96ad882be2d8162b064441649
Ankith SN <97029263+notankith@users.noreply.github.com>
2023-01-12 11:40:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/AM-ROBOTS/EvaMaria-V2 /EvaMaria-V2
+  git clone https://github.com/notankith/EvaMaria-V2 /EvaMaria-V2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria-V2
